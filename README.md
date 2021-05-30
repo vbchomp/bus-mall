5a25ef2ea11ff8ca74b74ae367823c0f32acaa5c
Heather B <vbchomp@gmail.com>
2021-05-29 20:21:27 -0700

    changes in css, js and readme. quitting for the day till my head can get back in the game. turning in as wip.

--- README.md
+++ README.md
@@ -7,9 +7,12 @@
 ## Authors
 
 - Heather Bisgaard, Software Engineer
+- Willem Jacobs
 
 ### References
 
 ### Reflections
 
 - Set-up. Setting up the repo, added all the json, reset and gitignore files, then created the index.html, style.css and app.js files. This is getting easier now that I create the files in VSCode and copy the rest of the reset files from the class repo. Dang, does this mean I am learning?
+
+- Lab 11. Went into this lab very confident, but it's amzaing how that confidence can shatter when you miss a few hours of classtime. I am trying to do the flexbox froggy and grid garden to work on CSS. But I think my JS needs some work, as I can't remember the functions needed to make the magic happen. Unfortunately, my head head has not been in the game this week due to personal issues at home. I just hope my head can get screwed back on soon.
--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 let allProducts = [];
 let clicks = 0;
 let clicksAllowed = 25;
+let sameNumbers = [];
 
 // accessing the DOM
 let myContainer = document.querySelector('section');
@@ -13,8 +14,7 @@ let imageTwo = document.querySelector('section img:nth-child(2)');
 let imageThree = document.querySelector('section img:last-child');
 
 // constructor
-
-function Product (name, fileExtension = 'jpg', ){
+function Product(name, fileExtension = 'jpg',) {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
   this.clicks = 0;
@@ -22,36 +22,35 @@ function Product (name, fileExtension = 'jpg', ){
   allProducts.push(this);
 }
 
-new Product ('bag');
-new Product ('banana');
-new Product ('bathroom');
-new Product ('boots');
-new Product ('breakfast');
-new Product ('bubblegum');
-new Product ('chair');
-new Product ('cthulhu');
-new Product ('dog-duck');
-new Product ('dragon');
-new Product ('pen');
-new Product ('pet-sweep');
-new Product ('scissors');
-new Product ('shark');
-new Product ('sweep', 'png');
-new Product ('tauntaun');
-new Product ('unicorn');
-new Product ('water-can');
-new Product ('wine-glass');
 
 
-
-
-
+// new instances
+new Product('bag');
+new Product('banana');
+new Product('bathroom');
+new Product('boots');
+new Product('breakfast');
+new Product('bubblegum');
+new Product('chair');
+new Product('cthulhu');
+new Product('dog-duck');
+new Product('dragon');
+new Product('pen');
+new Product('pet-sweep');
+new Product('scissors');
+new Product('shark');
+new Product('sweep', 'png');
+new Product('tauntaun');
+new Product('unicorn');
+new Product('water-can');
+new Product('wine-glass');
 
 
 // assets
-// imageOne.src = 'img/bag.jpg';
-// imageTwo.src = 'img/banana.jpg';
-// imageThree.src = 'img/bathroom.jpg';
+
+imageOne.src = 'img/bag.jpg';
+imageTwo.src = 'img/banana.jpg';
+imageThree.src = 'img/bathroom.jpg';
 // imageFour.src = 'img/boots.jpg';
 // imageFive.src = 'img/breakfast.jpg';
 // imageSix.src = 'img/bubblegum.jpg';
@@ -68,3 +67,33 @@ new Product ('wine-glass');
 // imageSeventeen.src = 'img/unicorn.jpg';
 // imageEighteen.src = 'img/water-can.jpg';
 // imageNineteen.src = 'img/wine-glass.jpg';
+
+// function to select random numbers between 0 and 18, the numbers of the product images in the allProducts array
+function selectRandomProductIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+// function to select only unique numbers that are not in the sameNumbers array. this will not allow a product image to repeat more than once in every 6 images.
+function selectUniqueProductNumbers() {
+  while (sameNumbers.length < 6) {
+    let numbers = selectRandomProductIndex();
+    if (!sameNumbers.includes(numbers)) {
+      sameNumbers.push(numbers);
+    }
+  }
+}
+
+// function to assign a product image the latest unique number and push to the allProducts array and increments the views each product receives. 
+function renderProducts() {
+  selectUniqueProductNumbers();
+  let productOne = sameNumbers[0];
+  let productTwo = sameNumbers[1];
+  let productThree = sameNumbers[2];
+  imageOne.src = allProducts[productOne].src;
+  imageTwo.src = allProducts[productTwo].src;
+  imageThree.src = allProducts[productThree].src;
+  allProducts()
+}
+
+
+renderProducts();
